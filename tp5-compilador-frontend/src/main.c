80220800f8bfca88f3f86014fbfa27836b6e8288
julian-salinas <juliansalinasm@gmail.com>
2022-01-28 19:11:48 -0300

    correcciones en main

--- tp5-compilador-frontend/src/main.c
+++ tp5-compilador-frontend/src/main.c
@@ -5,20 +5,23 @@ int yylexerrs = 0;
 extern int yynerrs;
 int yysemerrs = 0;
 
-int main(){
+int main(int argc, const char *argv[]){
     lista_identificadores_declarados = NULL;
 
-    switch( yyparse() ) {
+    switch(yyparse()) {
         case 0: 
             printf("Compilación terminada con éxito\n");
             break;
+        
         case 1: 
             printf("Errores de compilación\n");
             break;
+        
         case 2:
             printf("Memoria insuficiente\n");
             break;
     }
     printf("Errores sintácticos: %d - Errores léxicos: %d - Errores semánticos: %d\n", yynerrs, yylexerrs, yysemerrs);
-    return EXIT_SUCCESS;
+
+    return 0;
 }
